202509aa99a761ba8f5384c2d9dfa6ad82622f36
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2021-08-25 13:04:46 -0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+include_once("../db2.php");
+$status["status"]="";
+$data=1;
+$select=$DB->prepare("SELECT pagesText FROM pages WHERE (:id)");
+$select->bindParam(':id', $data);
+$select->execute();
+$select=$select->fetch(PDO::FETCH_ASSOC);
+$result=json_encode($select);
+{
+    print($result);
+}
+?>
