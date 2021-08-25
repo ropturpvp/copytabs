073763bed7365c899745bf66fe4618417987ebf8
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2021-08-25 13:32:23 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,9 @@
 <?php
-$host=getenv('DB_HOST');
-$dbName=getenv('DB_NAME');
-$user=getenv('DB_USER');
-$password=getenv('DB_PASS');
-$DB = new PDO('mysql:host='.$host.';dbname='.$dbName.'', $user, $password);
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return config(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
+});
+config($host, $dbName, $user, $password)
+  $DB = new PDO('mysql:host='.$host.';dbname='.$dbName.'', $user, $password);
+  return $DB;
 ?>
