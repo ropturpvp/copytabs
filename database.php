4f26aab87953b8ccea9b912935b49eba135d8794
ropturpvp <59321351+ropturpvp@users.noreply.github.com>
2021-08-25 13:34:31 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$app->get('/', function() use($app) {
+$app->get('/api.php', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return config(getenv('DB_HOST'), getenv('DB_NAME'), getenv('DB_USER'), getenv('DB_PASS'));
 });
